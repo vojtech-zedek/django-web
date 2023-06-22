cc6426201ee00cfa14e03f934fe6f5557221a421
vojtech-zedek <it2032@sspu-opava.cz>
2023-06-22 05:31:22 +0200

    05 - dokoncene sablony ale hnusny vizual

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('overview/', views.overview, name='overview'),
+    path('detail/<int:agency_id>/', views.detail, name='detail'),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Flight, Agency
 
-# Create your views here.
+def home(request):
+    flights = Flight.objects.all()
+    return render(request, 'index.html', {'flights': flights})
+
+def overview(request):
+    agencies = Agency.objects.all()
+    return render(request, 'overview.html', {'agencies': agencies})
+
+def detail(request, agency_id):
+    agency = get_object_or_404(Agency, id=agency_id)
+    return render(request, 'detail.html', {'agency': agency})
+
+
+
+
